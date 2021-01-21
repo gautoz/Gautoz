344b62095bd88bf8bb0cca654c8b872dc5ff887a
Thomasorus <contact@thomasorus.com>
2021-01-21 23:22:27 +0100

    Finished builder

--- src/build.py
+++ src/build.py
@@ -6,37 +6,54 @@ import glob
 import re
 import argparse
 import sys
+from datetime import datetime
 mistune = __import__('mistune')
 
 # Checking for dev command
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "-dev", "--dev", help="Prints the supplied argument.", action="store_true")
+    "-prod", "--prod", help="Prints the supplied argument.", action="store_true")
 args = parser.parse_args()
 
-print(args.dev)
+print(args.prod)
 
 buildUrl = ""
-if args.dev == True:
+if args.prod == False:
     buildUrl = "/"
 else:
     buildUrl = "https://thomasorus.github.io/Gautoz/"
-    # Generates html files in the site folder, using the entries and the template.
-    # Also triggers the creation of the breadcrumb and the submenu
 
 
-def generateHtmlPages(siteFolder, entries, template):
+# Generates html files in the site folder, using the entries and the template.
+
+def generateHtmlPages(siteFolder, entries, template, subPagesList):
     for val in entries:
 
         pageTemplate = re.sub('pageTitle', val['title'], template)
-        pageTemplate = re.sub('pageBody', val['pageContent'], pageTemplate)
-        pageTemplate = re.sub('parentLink', val['parent'], pageTemplate)
-        pageTemplate = re.sub('parentString', val['folder'], pageTemplate)
+
+        # Checking if root file
+        if val["file"] == "index":
+            val["pageContent"] = val["pageContent"] + subPagesList
+            pageTemplate = re.sub('pageBody', val['pageContent'], pageTemplate)
+        else:
+            pageTemplate = re.sub('pageBody', val['pageContent'], pageTemplate)
+
+        # Checking if navigation has value
+        if val["parentUrl"] == "":
+            navHtml = "<nav><em>Revenir à <a href='" + buildUrl + \
+                "'>"+val['parentText'] + "</a></em></nav>"
+            pageTemplate = re.sub('pageNavigation', navHtml, pageTemplate)
+        else:
+            navHtml = "<nav><em>Revenir à <a href='" + \
+                "/" + val['parentUrl'] + "'>" + \
+                val['parentText'] + "</a></em></nav>"
+            pageTemplate = re.sub('pageNavigation', navHtml, pageTemplate)
+
         pageTemplate = re.sub('buildUrl', buildUrl, pageTemplate)
 
-        # Create matinales folder
-        folderExists = os.path.exists(siteFolder+val['parent'])
+        # Create sub folders
+        folderExists = os.path.exists(siteFolder+val['folder'])
         if folderExists == False:
             os.mkdir(siteFolder+val['folder'])
 
@@ -97,13 +114,17 @@ def createEntries(pages):
         pageContent = getPageContent(page)
 
         tempPage['slug'] = path["slug"]
+        tempPage['file'] = path['file']
         tempPage['folder'] = path["folder"]
-        tempPage['parent'] = path['parent']
+        tempPage['parentUrl'] = path['parentUrl']
+        tempPage['parentText'] = path['parentText']
         tempPage['date'] = path['date']
+        tempPage['usDate'] = path['usDate']
         tempPage['title'] = title
         tempPage['pageContent'] = pageContent
 
         fullContent.append(tempPage)
+
     return fullContent
 
 # Recursively gather all files locations into an array
@@ -149,9 +170,59 @@ def cleanPath(path):
     pathItems = {}
     pathItems["slug"] = path + ".html"
     pathItems["folder"] = items[0]
-    pathItems["parent"] = "/" + items[0] + "/"
+    pathItems["file"] = items[1]
+    if pathItems["file"] == "index":
+        pathItems["parentUrl"] = ""
+        pathItems["parentText"] = "l'accueil"
+    else:
+        pathItems["parentUrl"] = items[0]
+        pathItems["parentText"] = items[0]
     pathItems["date"] = items[1]
+    if pathItems["date"] != "index":
+        pathItems["usDate"] = str(
+            datetime.strptime(pathItems["date"], '%d-%m-%Y'))
+    else:
+        pathItems["usDate"] = str(datetime.strptime("01-01-2000", '%d-%m-%Y'))
     return pathItems
+
+
+def generateSubPages(entries, num, folder, title):
+    entries.sort(key=lambda x: x["usDate"], reverse=True)
+    selectedEntries = entries[:num]
+    subPageList = "<ul class='listing'>"
+    for entry in selectedEntries:
+        if title == True:
+            linkUrl = entry["slug"]
+        else:
+            linkUrl = entry["file"] + ".html"
+
+        if entry["file"] != "index":
+            entryString = "<li><a href='" + \
+                linkUrl + "'>" + entry["date"] + \
+                " : " + entry["title"] + "</a></li>\n"
+            subPageList = subPageList + entryString
+    subPageList += "</ul>"
+
+    if title == True:
+        title = "<h2>" + folder.capitalize() + \
+            "</h2>"
+        subPageList = title + subPageList
+        subPageLink = "<small><a href='/" + folder + "'>Voir tout</a></small>"
+        subPageList += subPageLink
+    return subPageList
+
+
+def createHomePage(template, siteFolder):
+    homeFile = open("home.md", "r")
+    html = mistune.markdown(homeFile.read()) + "ContentList"
+
+    template = re.sub('pageTitle', "Accueil", template)
+    template = re.sub('pageBody', html, template)
+    template = re.sub('buildUrl', buildUrl, template)
+    template = re.sub('pageNavigation', "", template)
+
+    return template
+
 
 # Main function, generates the website
 
@@ -159,11 +230,25 @@ def cleanPath(path):
 def generateWebsite(siteFolder, contentFolder, templateFile, assetsPath):
     print('Welcome to the builder!')
     deleteWebsite(siteFolder)
-    pages = listPages(contentFolder)
-    entries = createEntries(pages)
     template = getHtmlTemplate(templateFile)
-    generateHtmlPages(siteFolder, entries, template)
-    moveAssets(siteFolder, assetsPath)
+    homePage = createHomePage(template, siteFolder)
+
+    for folder in contentFolder:
+        pages = listPages(folder + "/")
+        entries = createEntries(pages)
+        subPagesList = generateSubPages(entries, len(entries), folder, False)
+        homePageSubList = generateSubPages(entries, 5, folder, True)
+        homePage = re.sub('ContentList', homePageSubList +
+                          "ContentList", homePage)
+
+        generateHtmlPages(siteFolder, entries, template, subPagesList)
+        moveAssets(siteFolder, assetsPath)
+
+    homePage = re.sub('ContentList', "", homePage)
+    pageFile = open(siteFolder + "index.html", "w")
+    pageFile.write(homePage)
+    pageFile.close()
 
 
-generateWebsite('docs/', 'matinales/', 'partials/main.html', 'assets/')
+generateWebsite('docs/', ['matinales'],
+                'partials/main.html', 'assets/')
